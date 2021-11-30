e0a54a385b9b997808945dcdb6ba4b890878e4f4
Renan Souza <s.renan@outlook.com>
2021-11-30 18:58:47 -0300

    feat: implemment tests for Component Not Found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+const pikachuAlt = 'Pikachu crying because the page requested was not found';
+
+test('Testa se há um h2 com o texto Page requested not found', () => {
+  render(<NotFound />);
+  const headingNotFound = screen.getByRole('heading',
+    { level: 2, name: /page requested not found/i });
+  expect(headingNotFound).toBeInTheDocument();
+});
+
+test('Verifica se a imagem é renderizada na tela', () => {
+  render(<NotFound />);
+  const imageNotFound = screen.getByAltText(pikachuAlt);
+  expect(imageNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
