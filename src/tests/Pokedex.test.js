41de29b454c38ddb71a8e36d2c50d33ef9363f6d
Renan Souza <s.renan@outlook.com>
2021-12-03 17:47:16 -0300

    feat: implemment tests for Pokemon Details

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from '../renderWithRouter';
+import data from '../data';
+import App from '../App';
+
+const pokemons = data;
+const isPokemonFavoriteById = {
+  [pokemons[0].id]: false,
+  [pokemons[1].id]: false,
+};
+const types = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+
+// Feito com ajuda da Samantha na monitoria
+test('Testa se a página tem um h2 com o texto Encountered pokémons', () => {
+  renderWithRouter(<Pokedex
+    pokemons={ pokemons }
+    isPokemonFavoriteById={ isPokemonFavoriteById }
+  />);
+  const headingEl = screen.getByRole('heading',
+    { level: 2, name: /encountered pokémons/i });
+  expect(headingEl).toBeInTheDocument();
+});
+
+test('Teste se exibe próximo pokémon ao clicar no botão', () => {
+  renderWithRouter(<Pokedex
+    pokemons={ pokemons }
+    isPokemonFavoriteById={ isPokemonFavoriteById }
+  />);
+  const nextButton = screen.getByTestId('next-pokemon');
+  expect(nextButton).toHaveTextContent('Próximo pokémon');
+
+  userEvent.click(nextButton);
+});
+
+test('Teste se a Pokédex tem botões de filtro', () => {
+  renderWithRouter(<App />);
+
+  const maxLength = 7;
+  const typeButton = screen.getByRole('button');
+  expect(typeButton).toHaveLength(maxLength);
+
+  userEvent.click(typeButton);
+
+  const clearButton = screen.getByText('All');
+  expect(clearButton).toBeInTheDocument();
+
+  expect(pokemonType).toBe('Psychic');
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,59 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Testa se as informações do Pokémon selecionado são mostradas na tela.', () => {
+  renderWithRouter(<App />);
+
+  const linkDetails = screen.getByText(/more details/i);
+  expect(linkDetails).toBeInTheDocument();
+
+  userEvent.click(linkDetails);
+
+  const pokemonName = screen.getByText(/Pikachu Details/i);
+  expect(pokemonName).toBeInTheDocument();
+
+  const headingEl = screen.getByRole('heading', { level: 2, name: /summary/i });
+  // const headingEl = screen.getByText(/summary/i);
+  expect(headingEl).toBeInTheDocument();
+
+  const description = screen.getByText(/This intelligent Pokémon/i);
+  expect(description).toBeInTheDocument();
+});
+
+test('Testa se existe mapas com a localização do pokémon', () => {
+  renderWithRouter(<App />);
+
+  const linkDetails = screen.getByText(/more details/i);
+  userEvent.click(linkDetails);
+
+  const headingEl = screen.getByRole('heading',
+    { level: 2, name: 'Game Locations of Pikachu' });
+  expect(headingEl).toBeInTheDocument();
+
+  const locationName = screen.getByText(/Kanto Viridian Forest/i);
+  expect(locationName).toBeInTheDocument();
+
+  const mapImg = screen.getAllByAltText(/pikachu location/i);
+  expect(mapImg[0]).toBeInTheDocument();
+  expect(mapImg[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+});
+
+test('Testa se o usuári pode favoritar o Pokémon', () => {
+  renderWithRouter(<App />);
+
+  const linkDetails = screen.getByText(/more details/i);
+  userEvent.click(linkDetails);
+
+  const favoriteBtn = screen.getByLabelText(/pokémon favoritado?/i);
+  expect(favoriteBtn).toBeInTheDocument();
+  userEvent.click(favoriteBtn);
+
+  const favoriteLink = screen.getByText(/favorite pokémons/i);
+  userEvent.click(favoriteLink);
+
+  const pokemonCard = screen.getByText(/pikachu/i);
+  expect(pokemonCard).toBeInTheDocument();
+});
